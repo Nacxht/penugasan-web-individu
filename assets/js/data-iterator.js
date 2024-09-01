d3504c0ee58f5bc2716ab5929d1c4959da9da225
Nacxht <zombiecrafter069@gmail.com>
2024-09-01 18:52:33 +0700

    initial commit

---
+++ assets/js/data-iterator.js
@@ -0,0 +1,67 @@
+async function getFriendsBiodata() {
+  const data = fetch("../assets/data/data.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error happened:  " + response.statusText);
+      }
+
+      return response.json(); // Mengonversi respons menjadi JSON
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    });
+
+  return await data;
+}
+
+async function friendsBiodata() {
+  const friendsBiodata = document.getElementById("biodata-list");
+  const data = await getFriendsBiodata();
+  console.log(data);
+  const biodatas = `
+    ${data
+      .map(
+        (i) => `
+        <div class="biodata">
+            <img src="${i.fotoselfie}" alt="${i.nama}" class="avatar">
+
+            <table>
+                <tr>
+                    <td>Nama Lengkap</td>
+                    <td>:</td>
+                    <td>${i.nama}</td>
+                </tr>
+                <tr>
+                    <td>NIM</td>
+                    <td>:</td>
+                    <td>${parseInt(i.nim)}</td>
+                </tr>
+                <tr>
+                    <td>TTL</td>
+                    <td>:</td>
+                    <td>${i.ttl}</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>:</td>
+                    <td>${i.alamat}</td>
+                </tr>
+                <tr>
+                    <td>No. HP</td>
+                    <td>:</td>
+                    <td>${i.no}</td>
+                </tr>
+            </table>
+        </div>
+        `
+      )
+      .join("")}
+    `;
+
+  return (friendsBiodata.innerHTML = biodatas);
+}
+
+friendsBiodata();
